b5e99dc51b178aac5a12f28f1478bfaeb48f358b
tenore44 <tenore44@tenorcup.com>
2021-06-05 11:55:37 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
 # Create your views here.
