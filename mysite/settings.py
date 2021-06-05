46cda718b6be8250cbd3c9450800b5721389f8e2
tenore44 <tenore44@tenorcup.com>
2021-06-05 17:30:52 +0900

    change admin.site.title

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
+from django.contrib.admin import AdminSite
+
+AdminSite.site_header = 'DJBoys Blog'
 
 import os
 
